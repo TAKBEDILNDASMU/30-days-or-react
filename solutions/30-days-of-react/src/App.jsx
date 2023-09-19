1dc88bef84e9e3c16fa8b5ae06a8446a0fea9d33
Titan Apriliyan <titanaprilian73@gmail.com>
2023-09-19 19:34:12 +0700

    number generator exercise

--- solutions/30-days-of-react/src/App.jsx
+++ solutions/30-days-of-react/src/App.jsx
@@ -1,58 +1,50 @@
-import frontEnd from './assets/frontend_technologies.png'
-import rudeusImg from './assets/rudeus.png'
+/* eslint-disable react/prop-types */
+import "./App.css"
 
-function App() {
+const BoxNumber = ({ number }) => {
+  if (isPrime(number)) {
+    return (
+      <div className="box" style={{ backgroundColor: "red" }}>
+        {number}
+      </div>
+    )
+  }
+  if (number % 2 == 1) {
+    return (
+      <div className="box" style={{ backgroundColor: "lightgreen" }}>
+        {number}
+      </div>
+    )
+  } else {
+    return (
+      <div style={{ backgroundColor: "orange" }} className="box">
+        {number}
+      </div>
+    )
+  }
+}
 
-  const rudeus = {
-    firstName: "Rudeus",
-    lastName: 'Greyrat',
-    img: rudeusImg,
-    skills: ['Sorcerer', 'Healer', 'Sculptor'],
-    joined: "Sept 11 2023"
+function isPrime(n) {
+  if (n <= 1) return false
+
+  // Check from 2 to n-1
+  for (let i = 2; i < n; i++) if (n % i == 0) return false
+
+  return true
+}
+
+const App = () => {
+  let numbers = []
+  for (let i = 0; i <= 50; i++) {
+    numbers.push(i)
   }
 
-  const rudeusRenderedSkills = rudeus.skills.map((skill, i) => (
-    <li key={i}>{skill}</li>
-  ))
-
-  const frontendImg = (
-    <div className='img'>
-      <img src={frontEnd} alt="frontend tech" />
-    </div>
-  )
-
-  const subscribeForm = (
-    <form className='form' action="/" method="post">
-      <h2>SUBSCRIBE</h2>
-      <p>Sign up with your email address to receive news and update</p>
-      <div className="form-input">
-        <input type="text" placeholder='First name' />
-        <input type="text" placeholder='Last name' />
-        <input type="text" placeholder='Email' />
-      </div>
-      <button type="submit" className='btn'>Subscribe</button>
-    </form>
-  )
-
-  const userCard = (
-    <div className="card">
-      <img src={rudeus.img} alt="" />
-      <h2>{rudeus.firstName} {rudeus.lastName}</h2>
-      <p>Ahli Rawa, Doronuma</p>
-      <p>SKILLS</p>
-      <ul>
-        {rudeusRenderedSkills}
-      </ul>
-      <small>Joined on: {rudeus.joined}</small>
-    </div>
-  )
-
   return (
-    <>
-     {frontendImg}
-     {subscribeForm}
-     {userCard}
-    </>
+    <div className="container">
+      {numbers.map((number) => (
+        <BoxNumber key={number} number={number} />
+      ))}
+    </div>
   )
 }
 
